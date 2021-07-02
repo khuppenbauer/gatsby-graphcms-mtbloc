ff8f9a45867e64ffa86a3df3a4e0b0cf0205b449
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-07-02 08:36:50 +0200

    TASK: Change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `MTB Loc`,
+        short_name: `MTB Loc`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#111827`,
+        theme_color: `#111827`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/noun_GPS_1461739.png`,
       },
     },
     {
