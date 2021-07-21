bfcfa8fb3528c65976ace306c6d498f65e2c06f4
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-07-21 07:49:31 +0200

    TASK: Adjust image styles

--- src/views/teaser.js
+++ src/views/teaser.js
@@ -3,10 +3,10 @@ import { Link } from "gatsby"
 import Image from "@graphcms/react-image";
 
 const Teaser = ({ slug, title, image }) => (
-  <div className="p-4 md:w-1/3">
+  <div className="p-4 md:w-1/3 xl:w-1/5 w-full">
     <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
       <Link to={slug}>
-        {image ? <Image image={image} maxWidth={320} /> : null}
+        {image ? <Image image={image} className="w-full object-cover object-center" /> : null}
         <div className="p-6">
           <h1 className="title-font text-lg font-medium text-white mb-3">
             {title}
