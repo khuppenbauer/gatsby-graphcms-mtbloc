131cb9783f9075af4d6bfae2d99dc5e01ebbd38f
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-07-23 08:26:04 +0200

    TASK: Add static image map to regions list

--- src/pages/regions.js
+++ src/pages/regions.js
@@ -1,11 +1,61 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import getBounds from 'geolib/es/getBounds';
+import getCenterOfBounds from 'geolib/es/getCenterOfBounds';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Section from "../components/section"
 import Teaser from "../views/teaser"
 import Headline from "../views/headline"
+
+const mapboxToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
+const mapboxBaseUrl = 'https://api.mapbox.com/styles/v1/';
+const mapboxStyle = 'khuppenbauer/ckrd352a31ffl18qit94pozv8';
+const imageSize = '320x240';
+const stroke = '#ff3300';
+const strokeWidth = 2;
+const fillOpacity = 0;
+
+const createImage = (tracks) => {
+  const coords = tracks.reduce((acc, track) => {
+    acc.push(track.minCoords);
+    acc.push(track.maxCoords);
+    return acc;
+  }, []);
+  const bounds = getBounds(coords);
+  const center = getCenterOfBounds(coords);
+  const { minLat, minLng, maxLat, maxLng } = bounds;
+  const { latitude, longitude } = center;
+  const geoJsonString = {
+    type: 'Feature',
+    properties: {
+      stroke,
+      'stroke-width': strokeWidth,
+      'fill-opacity': fillOpacity,
+    },
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [minLng, minLat],
+          [maxLng, minLat],
+          [maxLng, maxLat],
+          [minLng, maxLat],
+          [minLng, minLat]
+        ]
+      ]
+    },
+  };
+  const pathParams = [
+    mapboxStyle,
+    'static',
+    `geojson(${encodeURIComponent(JSON.stringify(geoJsonString))})`,
+    `${longitude},${latitude},6`,
+    imageSize,
+  ];
+  return `${mapboxBaseUrl}${pathParams.join('/')}?access_token=${mapboxToken}`;
+}
 
 const RegionsPage = ({ data: { countries } }) => {
   return (
@@ -21,8 +71,9 @@ const RegionsPage = ({ data: { countries } }) => {
                 {regions.map(region => {
                   const { id: regionId, name, tracks, gatsbyPath } = region
                   const tracksCount = tracks.length;
+                  const image = createImage(tracks);
                   return tracks.length > 0 ? (
-                    <Teaser key={regionId} slug={gatsbyPath} title={`${name} (${tracksCount})`} />
+                    <Teaser key={regionId} slug={gatsbyPath} title={`${name} (${tracksCount})`} asset={image} />
                   ) : null
                 })}
               </div>
@@ -46,6 +97,14 @@ export const pageQuery = graphql`
           name
           tracks {
             id
+            minCoords {
+              latitude
+              longitude
+            }
+            maxCoords {
+              latitude
+              longitude
+            }
           }
         }
       }
--- src/views/teaser.js
+++ src/views/teaser.js
@@ -2,11 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Image from "@graphcms/react-image";
 
-const Teaser = ({ slug, title, image }) => (
+const Teaser = ({ slug, title, image, asset }) => (
   <div className="p-4 md:w-1/3 xl:w-1/5 w-full">
     <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
       <Link to={slug}>
         {image ? <Image image={image} className="w-full object-cover object-center" /> : null}
+        {asset ? <img className="w-full object-cover object-center" src={asset} alt={title} width="320" height="240" /> : null}
         <div className="p-6">
           <h1 className="title-font text-lg font-medium text-white mb-3">
             {title}
