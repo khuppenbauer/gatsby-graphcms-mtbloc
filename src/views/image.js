7fd1c510067ea793771a3050d6f036f201ec2a22
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-09-04 10:18:52 +0200

    TASK: Use simple image slider

--- src/views/image.js
+++ src/views/image.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { Image as ImageIcon, MapPin as MapIcon } from "react-feather"
 import slugify from '@sindresorhus/slugify';
 
 const assetBaseUrl = process.env.GATSBY_ASSET_BASE_URL
+
+const ImageSlider = ({ images }) => {
+  const [index, setIndex] = useState(0);
+
+  const slide = () => {
+    setIndex((index + 1) % images.length);
+  };
+
+  let button = (
+    <button className="rounded-full h-10 w-10 bg-gray-900 flex items-center justify-center" onClick={slide}>
+      <MapIcon className="w-5 h-5"/>
+    </button>
+  );
+  if (index === 1) {
+    button = (
+      <button className="rounded-full h-10 w-10 bg-gray-900 flex items-center justify-center" onClick={slide}>
+        <ImageIcon className="w-5 h-5"/>
+      </button>
+    );
+  }
+  return (
+    <div key={images[index].key} id={images[index].id} className="relative w-full carousel-item">
+      <img className="w-full object-cover object-center" src={images[index].src} alt={images[index].title} width="320" height="240" />
+      <div className="absolute flex justify-between transform right-2 bottom-2">
+        {button}
+      </div>
+    </div> 
+  )
+};
 
 const Image = ({ id, slug, title, image, staticImage }) => {
   let assets = [];
@@ -13,6 +42,7 @@ const Image = ({ id, slug, title, image, staticImage }) => {
       key: imageId,
       id: slugify(`${id}-${handle}`),
       src: `${assetBaseUrl}/resize=w:320,h:240,fit:crop/auto_image/compress/${handle}`,
+      title,
     });
   }
   if (staticImage) {
@@ -21,35 +51,13 @@ const Image = ({ id, slug, title, image, staticImage }) => {
       key: imageId,
       id: slugify(`${id}-${handle}`),
       src: `${assetBaseUrl}/resize=w:320,h:240,fit:crop/auto_image/compress/${handle}`,
+      title,
     });
   }
+
   return (
     assets.length > 1 ? (
-      <div className="carousel">
-        {assets.map((asset, index) => {
-          const { key, src, id } = asset;
-          let button = (
-            <a href={`#${assets[1].id}`} className="btn btn-circle">
-              <MapIcon className="w-5 h-5"/>
-            </a>
-          );
-          if (index === 1) {
-            button = (
-              <a href={`#${assets[0].id}`} className="btn btn-circle">
-                <ImageIcon className="w-5 h-5"/>
-              </a>
-            );
-          }
-          return (
-            <div key={key} id={id} className="relative w-full carousel-item">
-              <img className="w-full object-cover object-center" src={src} alt={title} width="320" height="240" />
-              <div className="absolute flex justify-between transform right-2 bottom-2">
-                {button}
-              </div>
-            </div> 
-          )
-        })}
-      </div>
+      <ImageSlider images={assets} />
     ) : (
       <Link to={slug}>
         <img className="w-full object-cover object-center" src={assets[0].src} alt={title} width="320" height="240" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [],
 }
