c800ad3af8632710671c9999e9a86a3c3ac0a60f
Kerstin Huppenbauer <k.huppenbauer@web.de>
2022-05-04 07:59:40 +0200

    TASK: Use markdown instead of html for collection description

--- src/views/header.js
+++ src/views/header.js
@@ -1,4 +1,6 @@
 import React from "react"
+import ReactMarkdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
 import { Link } from "gatsby"
 
 const Header = ({ title, description, additionalLink }) => (
@@ -12,12 +14,14 @@ const Header = ({ title, description, additionalLink }) => (
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">{title}</h1>
     </div>
     {description ? (
-      <>
-        <div 
-          className="w-full mb-t lg:mt-0 leading-relaxed text-gray-400 text-opacity-90 pt-4"
-          dangerouslySetInnerHTML={{__html: description.html}} 
+      <ReactMarkdown
+        className="w-full mb-t lg:mt-0 leading-relaxed text-gray-400 text-opacity-90 pt-4"
+        children={description.markdown}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          a: ({node, ...props}) => <a className='text-blue-400' {...props} />
+        }}
         />
-      </>
     ) : null}
   </div>
 )
